d7e588bdfbb2c4171b98aaf0935fea683e63b835
LiuHuan <16281044@bjtu.edu.cn>
2018-08-31 12:37:00 +0800

    增加了搜索下载功能

--- GetUrl.py
+++ GetUrl.py
@@ -4,6 +4,25 @@ from random import random
 from time import time
 import json
 
+def menu(self):
+    print("0. 一键下载网易云音乐我喜欢歌单")
+    print("1. 手动搜索并下载音乐")
+    cho = input("请选择你想要的操作：")
+    if cho == "0":
+        pass
+    if cho == "1":
+        searchanddownload()
+
+
+def searchanddownload():
+    key_word = input("请输入你想搜索的歌曲名称：")
+    l = search_song(key_word)
+    f_l = choose_music(l)
+    add_vkey = get_vkey(f_l)
+    s_u_l = get_music_url(add_vkey)
+    SaveFile(s_u_l)
+
+
 def search_song(self, key_word, page=1, num=20):
         ''' 根据关键词查找歌曲 '''
         url = 'https://c.y.qq.com/soso/fcgi-bin/client_search_cp'
@@ -24,10 +43,20 @@ def search_song(self, key_word, page=1, num=20):
             guid = int(random() * 2147483647) * int(time() * 1000) % 10000000000
             item['guid']=guid
             song_list.append(item)
-            # song = Song(media_mid=media_mid, song_mid=song_mid,
-            #             title=title, singer=singer, album=album, data=line)
-            # song_list.append(song)
-        return song_list[0]
+        return song_list
+
+def choose_music(song_list):
+    index = 0
+    for song in song_list:
+        print(index+'. ', song['title']+" ", song['singer']+" ", song['album'])
+        index++
+    s = input("请选择你想要下载的音乐（多选请用空格隔开）：")
+    choose = str(s).split(' ')
+    res = []
+    for i in choose:
+        res.append(song_list[int(i)])
+    return res
+
 
 def get_vkey(song_list):
     song_list_vkey=[]
@@ -38,14 +67,14 @@ def get_vkey(song_list):
         rst = requests.get(url)
         vkey = json.loads(rst.text)['data']['items'][0]['vkey']
         item['vkey']=vkey
-        song_list.append(item)
+        song_list_vkey.append(item)
     return song_list_vkey
 
 def get_music_url(song_list_vkey):
     song_url_list=[]
     for item in song_list_vkey:
         url = 'http://dl.stream.qqmusic.qq.com/%s?' % item['filename']
-        music_url = url + 'vkey=%s&guid=%s&fromtag=30' % (item['vkey'], item['guid'])
+        music_url = url + 'vkxey=%s&guid=%s&fromtag=30' % (item['vkey'], item['guid'])
         item['music_url']=music_url
         song_url_list.append(item)
     return song_url_list
@@ -54,6 +83,3 @@ def SaveFile(song_url_list):
     with open('index.txt','w+') as f:
         for item in song_url_list:
             f.write(item['title']+'_'+item['singer']+'|'+item['music_url'])
-    
-
-
